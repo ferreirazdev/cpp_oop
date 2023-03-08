0862ac75b3fefb7a07ef1985d39df875f9ad1816
ferreirazdev <business.flavioferreira@gmail.com>
2023-03-08 08:40:34 -0300

    feat: getters and setters

--- getter_setter/getter_setter.cpp
+++ getter_setter/getter_setter.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 
+class Stove {
+    private:
+        int temperature = 0;
+    public:
+        int getTemperature() {
+            return this->temperature;
+        }
+        void setTemperature(int temperature) {
+            if(temperature < 0){
+                this->temperature = 0;
+            }
+            else if(temperature >= 10){
+                this->temperature = 10;
+            }
+            else{
+                this->temperature = temperature;
+            }
+        }
+};
+
 int main() {
 
-    
+    Stove stove;
+
+    stove.setTemperature(5);
+    std::cout << "The temperature setting is: " << stove.getTemperature();
 
     return 0;
 }
